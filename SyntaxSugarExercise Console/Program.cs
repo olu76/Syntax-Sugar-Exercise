038fd169a98b287d5762d2a6d4a3202a303cc2e7
olumide azeez afolabi <midas44000@yahoo.com>
2021-11-04 01:50:21 -0500

    initial commit

---
+++ SyntaxSugarExercise Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SyntaxSugarExercise_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var answer = 4;
+
+            //var response = (answer < 9) ? $"{answer} is less than {9}" : $"{9} is greater than or equal to  {9}";
+
+            //Console.WriteLine((answer < 9) ? $"{answer} is less than {9}" : $"{9} is greater than or equal to {9}");
+
+            //if (answer < 9)
+            //{
+            //    response = $"{answer} is less than {9}";
+            //}
+            //else
+            //{
+            //    response = $" {answer} is greater than or equal {9}";
+
+
+        var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+            
+
+              
+         }
+
+    }
+}
+  
